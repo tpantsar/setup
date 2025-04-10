4c42e213c7c14bc3f684afcf882aae4660bed318
tpantsar <tomi.pantsar@gmail.com>
2025-04-10 14:26:10 +0300

    Modify run.sh to Ubuntu/Debian systems

--- run.sh
+++ run.sh
@@ -6,8 +6,8 @@ print_logo() {
     ______                _ __    __     
    / ____/______  _______(_) /_  / /__   
   / /   / ___/ / / / ___/ / __ \/ / _ \  
- / /___/ /  / /_/ / /__/ / /_/ / /  __/  Arch Linux System Crafting Tool
- \____/_/   \__,_/\___/_/_.___/_/\___/   by: typecraft
+ / /___/ /  / /_/ / /__/ / /_/ / /  __/  Ubuntu/Debian System Crafting Tool
+ \____/_/   \__,_/\___/_/_.___/_/\___/
 
 EOF
 }
@@ -34,21 +34,7 @@ echo "Starting system setup..."
 
 # Update the system first
 echo "Updating system..."
-sudo pacman -Syu --noconfirm
-
-# Install yay AUR helper if not present
-if ! command -v yay &> /dev/null; then
-  echo "Installing yay AUR helper..."
-  sudo pacman -S --needed git base-devel --noconfirm
-  git clone https://aur.archlinux.org/yay.git
-  cd yay
-  echo "building yay.... yaaaaayyyyy"
-  makepkg -si --noconfirm
-  cd ..
-  rm -rf yay
-else
-  echo "yay is already installed"
-fi
+sudo apt update && sudo apt upgrade -y
 
 # Install packages by category
 echo "Installing system utilities..."
@@ -62,9 +48,6 @@ install_packages "${MAINTENANCE[@]}"
 
 echo "Installing desktop environment..."
 install_packages "${DESKTOP[@]}"
-
-echo "Installing desktop environment..."
-install_packages "${OFFICE[@]}"
 
 echo "Installing media packages..."
 install_packages "${MEDIA[@]}"
@@ -81,18 +64,25 @@ for service in "${SERVICES[@]}"; do
   else
     echo "$service is already enabled"
   fi
+  sudo systemctl start "$service"
 done
 
 # Install gnome specific things to make it like a tiling WM
-echo "Installing Gnome extensions..."
-. gnome/gnome-extensions.sh
-echo "Setting Gnome hotkeys..."
-. gnome/gnome-hotkeys.sh
-echo "Configuring Gnome..."
-. gnome/gnome-settings.sh
+if [ -d gnome ]; then
+  echo "Installing Gnome extensions..."
+  . gnome/gnome-extensions.sh
+
+  echo "Setting Gnome hotkeys..."
+  . gnome/gnome-hotkeys.sh
+
+  echo "Configuring Gnome..."
+  . gnome/gnome-settings.sh
+fi
 
 # Some programs just run better as flatpaks. Like discord/spotify
-echo "Installing flatpaks (like discord and spotify)"
-. install-flatpaks.sh
+if [ -f install-flatpaks.sh ]; then
+  echo "Installing flatpaks (like discord and spotify)"
+  . install-flatpaks.sh
+fi
 
 echo "Setup complete! You may want to reboot your system."
