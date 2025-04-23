7b1b67f4716ccd11377b6bed8a6a9b17c798996b
tpantsar <tomi.pantsar@gmail.com>
2025-04-23 12:12:02 +0300

    gnome input-sources

--- gnome/dump.sh
+++ gnome/dump.sh
@@ -9,4 +9,8 @@ dconf dump /org/gnome/shell/extensions/ > "$SCRIPT_DIR/gnome-extensions.dconf"
 echo "Dumping GNOME peripherals settings..."
 dconf dump /org/gnome/desktop/peripherals/ > "$SCRIPT_DIR/gnome-peripherals.dconf"
 
+# Input sources
+echo "Dumping GNOME input sources settings..."
+dconf dump /org/gnome/desktop/input-sources/ > "$SCRIPT_DIR/gnome-input-sources.dconf"
+
 echo "Dumped GNOME settings to $SCRIPT_DIR/gnome-extensions.dconf and $SCRIPT_DIR/gnome-peripherals.dconf."
--- gnome/load.sh
+++ gnome/load.sh
@@ -9,4 +9,8 @@ dconf load /org/gnome/shell/extensions/ < "$SCRIPT_DIR/gnome-extensions.dconf"
 echo "Loading GNOME peripherals settings..."
 dconf load /org/gnome/desktop/peripherals/ < "$SCRIPT_DIR/gnome-peripherals.dconf"
 
+# Input sources
+echo "Loading GNOME input sources settings..."
+dconf load /org/gnome/desktop/input-sources/ < "$SCRIPT_DIR/gnome-input-sources.dconf"
+
 echo "Loaded GNOME settings to $SCRIPT_DIR/gnome-extensions.dconf and $SCRIPT_DIR/gnome-peripherals.dconf."
