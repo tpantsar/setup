1bd144df85e61d88c019ab07bbed97a47a159e50
tpantsar <tomi.pantsar@gmail.com>
2025-04-23 12:52:50 +0300

    shell/app-switcher

--- gnome/settings/dump.sh
+++ gnome/settings/dump.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# Shell
+EXTENSIONS="$SCRIPT_DIR/shell/extensions.dconf"
+APP_SWITCHER="$SCRIPT_DIR/shell/app-switcher.dconf"
+
 # Desktop
 PERIPHERALS="$SCRIPT_DIR/desktop/peripherals.dconf"
 INPUT_SOURCES="$SCRIPT_DIR/desktop/input-sources.dconf"
 
-# Shell
-EXTENSIONS="$SCRIPT_DIR/shell/extensions.dconf"
-
 echo "Dumping GNOME settings..."
 
 dconf dump /org/gnome/shell/extensions/ > $EXTENSIONS
+dconf dump /org/gnome/shell/app-switcher/ > "$APP_SWITCHER"
 dconf dump /org/gnome/desktop/peripherals/ > "$PERIPHERALS"
 dconf dump /org/gnome/desktop/input-sources/ > "$INPUT_SOURCES"
 
 echo "Dumped GNOME settings to:"
 echo "  $EXTENSIONS"
+echo "  $APP_SWITCHER"
 echo "  $PERIPHERALS"
 echo "  $INPUT_SOURCES"
