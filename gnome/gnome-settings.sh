2ca5349e55aafef81374d724791b4762b1ba50bd
tpantsar <tomi.pantsar@gmail.com>
2025-04-23 12:07:15 +0300

    set xkb-options like remapping caps lock to esc

--- gnome/gnome-settings.sh
+++ gnome/gnome-settings.sh
@@ -7,3 +7,6 @@ gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
 echo "setting wallpaper to ${SCRIPT_DIR}/wallpaper.jpeg"
 gsettings set org.gnome.desktop.background picture-uri-dark "file://${SCRIPT_DIR}/wallpaper.jpeg"
 gsettings set org.gnome.desktop.background picture-options 'zoom'
+
+# Input sources
+gsettings set org.gnome.desktop.input-sources xkb-options "['caps:escape', 'grp:win_space_toggle', 'terminate:ctrl_alt_bksp']"
