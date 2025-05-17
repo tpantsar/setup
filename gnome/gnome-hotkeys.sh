ce8be9823b83b240338892e5562cae35388b5da6
tpantsar <tomi.pantsar@gmail.com>
2025-05-17 13:15:09 +0300

    gnome keybinds

--- gnome/gnome-hotkeys.sh
+++ gnome/gnome-hotkeys.sh
@@ -1,6 +1,6 @@
-# Use 6 fixed workspaces instead of dynamic mode
+# Use 8 fixed workspaces instead of dynamic mode
 gsettings set org.gnome.mutter dynamic-workspaces false
-gsettings set org.gnome.desktop.wm.preferences num-workspaces 6
+gsettings set org.gnome.desktop.wm.preferences num-workspaces 8
 
 # Unset super+number because for some reason gnome silently changes them?
 gsettings set org.gnome.shell.keybindings switch-to-application-1 "[]"
@@ -13,13 +13,5 @@ gsettings set org.gnome.shell.keybindings switch-to-application-7 "[]"
 gsettings set org.gnome.shell.keybindings switch-to-application-8 "[]"
 gsettings set org.gnome.shell.keybindings switch-to-application-9 "[]"
 
-# Use super for workspaces
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<Super>1']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['<Super>2']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 "['<Super>3']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4 "['<Super>4']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-5 "['<Super>5']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-6 "['<Super>6']"
-
-# Alt+F4 is very cumbersome
-gsettings set org.gnome.desktop.wm.keybindings close "['<Super>c']"
+# Close windows
+gsettings set org.gnome.desktop.wm.keybindings close "['<Super>q']"
