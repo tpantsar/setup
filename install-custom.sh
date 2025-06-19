a0093348fc6a4cc76306e6a0fcbe400a9d12a85e
tpantsar <tomi.pantsar@gmail.com>
2025-06-19 13:29:47 +0300

    Add symlink to kitty executable

--- install-custom.sh
+++ install-custom.sh
@@ -55,9 +55,15 @@ else
 fi
 
 # Install kitty - https://sw.kovidgoyal.net/kitty/binary/
+# Do not copy the kitty binary out of the installation folder.
+# If you want to add it to your PATH, create a symlink in ~/.local/bin or /usr/bin or wherever.
 if ! command -v kitty &> /dev/null; then
   echo "Installing kitty..."
   curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+
+  # Add symlink to kitty executable
+  ln -s ~/.local/kitty.app/bin/kitty ~/.local/bin/kitty
+  kitty --version
 else
   echo "kitty is already installed"
 fi
