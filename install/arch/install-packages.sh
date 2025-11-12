331dcd8b84d323c4f3f0377069398deef69edf8b
tpantsar <tomi.pantsar@gmail.com>
2025-11-12 18:57:34 +0200

    add nvm and yarn to packages

--- install/arch/install-packages.sh
+++ install/arch/install-packages.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo pacman -S --noconfirm --needed wget github-cli zsh stow tmux firefox npm uv tree thunderbird xclip texlive-binextra keepassxc
+sudo pacman -S --noconfirm --needed wget github-cli zsh stow tmux firefox npm nvm yarn uv tree thunderbird xclip texlive-binextra keepassxc
 yay -S --noconfirm --needed onedrive-abraunegg sesh-bin
