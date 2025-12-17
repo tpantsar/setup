4b20e04615d7d859b799ecb6a2abfa3fe6faefcf
tpantsar <tomi.pantsar@gmail.com>
2025-12-17 10:32:42 +0200

    Tmux install from source

--- install/install-tmux.sh
+++ install/install-tmux.sh
@@ -3,12 +3,25 @@
 source /etc/os-release
 set -e
 
+# Troubleshooting:
+# tmux open terminal failed: not a terminal
+# https://stackoverflow.com/questions/25207909/tmux-open-terminal-failed-not-a-terminal
+
 # Install tmux
 if [[ "$ID" == "arch" ]]; then
   yay -S --noconfirm --needed tmux
 elif [[ "$ID" == "ubuntu" || "$ID" == "debian" ]]; then
-  sudo apt-get update
-  sudo apt-get install -y tmux
+  # sudo apt-get update
+  # sudo apt-get install -y tmux
+  # https://github.com/tmux/tmux/wiki/Installing#from-source-tarball
+  # https://github.com/tmux/tmux/wiki/Installing#from-version-control
+  git clone https://github.com/tmux/tmux.git ~/tmux-installation
+  cd ~/tmux-installation
+  sudo apt install -y autoconf automake pkg-config libevent-dev ncurses-dev build-essential bison
+  sh autogen.sh
+  ./configure --enable-static
+  make && sudo make install
+  tmux -V
   echo "tmux installed successfully!"
 else
   echo "Other distro: $ID"
