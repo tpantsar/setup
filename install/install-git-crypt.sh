09c11a969e8e47cf4d9150081b07b79d296156a9
tpantsar <tomi.pantsar@gmail.com>
2026-01-15 11:33:26 +0200

    fix git-crypt install

--- install/install-git-crypt.sh
+++ install/install-git-crypt.sh
@@ -3,10 +3,13 @@ if ! command -v git-crypt &>/dev/null; then
   echo "Installing git-crypt ..."
   TARBALL="/tmp/git-crypt.tar.gz"
   curl -Lo ${TARBALL} "https://www.agwa.name/projects/git-crypt/downloads/git-crypt-0.8.0.tar.gz"
-  tar -xzf "$TARBALL"
+  tar -xzf "$TARBALL" -C /tmp
   cd /tmp/git-crypt-*
   make
   sudo make install PREFIX=/usr/local
+  rm -rf /tmp/git-crypt-*
+  git-crypt --version
+  echo "git-crypt installed successfully."
 else
   echo "git-crypt is already installed"
 fi
