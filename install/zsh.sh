526d7c014f92a88f0e2ad5acc9895f891f6091cc
tpantsar <tomi.pantsar@gmail.com>
2026-03-28 11:03:21 +0200

    zsh plugins

--- install/zsh.sh
+++ install/zsh.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-
-if [ -d "$HOME/.oh-my-zsh" ]; then
-  echo "Oh My Zsh is already installed. Skipping installation."
-  exit 0
-fi
+# https://gist.github.com/n1snt/2cccc8aa5f7b645a7628d3512c70deb6
 
 # oh-my-zsh - https://ohmyz.sh/#basic-installation
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+if [ -d "$HOME/.oh-my-zsh" ] && command -v omz >/dev/null 2>&1; then
+  echo "Oh My Zsh is already installed. Updating with omz update ..."
+  omz update
+else
+  echo "Installing Oh My Zsh ..."
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+# Install zinit (plugin manager)
+# https://github.com/zdharma-continuum/zinit?tab=readme-ov-file#install
+bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
