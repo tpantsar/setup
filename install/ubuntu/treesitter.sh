cbb827d87263e28c2700da8d44cfd1b31bc43819
tpantsar <tomi.pantsar@gmail.com>
2026-04-13 09:47:03 +0300

    treesitter

--- install/ubuntu/treesitter.sh
+++ install/ubuntu/treesitter.sh
@@ -5,6 +5,7 @@ if ! command -v cargo &>/dev/null; then
   sudo apt install -y cargo
 fi
 
+# https://github.com/tree-sitter/tree-sitter/blob/master/crates/cli/README.md
 if ! command -v tree-sitter >/dev/null 2>&1; then
   echo "Installing tree-sitter with cargo ..."
   cargo install --locked tree-sitter-cli
