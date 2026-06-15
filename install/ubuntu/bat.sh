b004b67e336d4a95235c6ddf00706f1ec681f80e
tpantsar <tomi.pantsar@gmail.com>
2026-06-15 13:53:33 +0300

    bat

--- install/ubuntu/bat.sh
+++ install/ubuntu/bat.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 # bat -> batcat symlink
-if ! command -v bat >/dev/null 2>&1 && command -v batcat >/dev/null 2>&1; then
+if ! command -v bat >/dev/null 2>&1 || ! command -v batcat >/dev/null 2>&1; then
+  echo "Installing bat from apt"
   sudo apt install -y bat
   ln -sf /usr/bin/batcat "$HOME/.local/bin/bat"
   echo "Symlink created: $HOME/.local/bin/bat -> /usr/bin/batcat"
   echo "bat installed and symlinked to batcat"
-  echo "bat version: $(bat --version)"
+else
+  echo "bat is already installed"
 fi
+
+echo "bat version: $(bat --version)"
