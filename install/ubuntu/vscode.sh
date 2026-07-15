1e7035fe7608cc41493e30e48c6fae3b23b7aee8
tpantsar <tomi.pantsar@gmail.com>
2026-07-15 12:44:58 +0300

    vscode

--- install/ubuntu/vscode.sh
+++ install/ubuntu/vscode.sh
@@ -2,12 +2,19 @@
 # Install VSCode latest version
 # https://code.visualstudio.com/docs/setup/linux#_install-vs-code-on-linux
 
+version() {
+  echo "vscode path: $(command -v code)"
+  echo "vscode version: $(code --version)"
+}
+
 if command -v code &>/dev/null; then
-  echo "VSCode is already installed. Skipping."
+  echo "vscode is already installed. Skipping."
+  version
   exit 0
 fi
 
-sudo apt-get install wget gpg
+sudo apt update
+sudo apt install -y wget gpg
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor >microsoft.gpg
 sudo install -D -o root -g root -m 644 microsoft.gpg /usr/share/keyrings/microsoft.gpg
 rm -f microsoft.gpg
@@ -21,8 +28,7 @@ Architectures: amd64,arm64,armhf
 Signed-By: /usr/share/keyrings/microsoft.gpg
 EOF
 
-sudo apt install apt-transport-https
-sudo apt update
-sudo apt install code
+sudo apt install -y apt-transport-https code
 
-code --version
+echo "vscode installed successfully!"
+version
